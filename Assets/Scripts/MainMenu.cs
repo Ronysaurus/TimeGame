2445461c0919af7f33e8ff067b2be73fdd168990
Alain <barraganalain55@gmail.com>
2019-10-10 17:55:24 -0500

    menu principal layout

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+    }
+
+    public void LevelSelect()
+    {
+    }
+}
