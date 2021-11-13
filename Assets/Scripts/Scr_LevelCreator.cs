3c14aca718849ee3dbcdd40be9d589fff86b72c1
Alain <barraganalain55@gmail.com>
2021-11-12 18:01:38 -0600

    Level Creator

---
+++ Assets/Scripts/Scr_LevelCreator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class Scr_LevelCreator : MonoBehaviour
+{
+    JsonData jsonData;
+
+    public GameObject normalTile;
+
+    private void Start()
+    {
+        ReadFile();
+        Create();
+    }
+
+    void ReadFile()
+    {
+        StreamReader reader = new StreamReader("Assets/Resources/test.json");
+        jsonData = JsonMapper.ToObject(reader.ReadToEnd());
+        reader.Close();
+    }
+
+    void Create()
+    {
+        int numTiles = jsonData["Tiles"].Count;
+        for (int i = 0; i < numTiles; i++)
+        {
+            switch (int.Parse(jsonData["Tiles"][i]["Type"].ToString()))
+            {
+                case 0:
+                    {
+                        GameObject.Instantiate(normalTile, new Vector3(float.Parse(jsonData["Tiles"][i]["posX"].ToString()), float.Parse(jsonData["Tiles"][i]["posY"].ToString())), Quaternion.identity);
+                        break;
+                    }
+            }
+        }
+    }
+}
